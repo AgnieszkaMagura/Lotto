6114d7c95f06592d473691db86b7a6a972577f43
Agnieszka Magura <agnieszka.magura@gmail.com>
2026-02-01 13:02:54 +0000

    added more configuration and added steps into first feature class

--- src/integration/java/pl.lotto/BaseIntegrationTest.java
+++
@@ -1,9 +0,0 @@
-package pl.lotto;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import pl.lotto.domain.LottoSpringBootApplication;
-
-@SpringBootTest(classes = LottoSpringBootApplication.class)
-public class BaseIntegrationTest {
-
-}
