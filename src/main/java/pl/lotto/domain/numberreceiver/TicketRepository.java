6defeb11f87053babed72e229be1d32cab1dabe3
Agnieszka Magura <agnieszka.magura@gmail.com>
2026-02-28 11:58:04 +0000

    fix(resultannouncer): implement Serializable for ResponseDto

--- src/main/java/pl/lotto/domain/numberreceiver/TicketRepository.java
+++ src/main/java/pl/lotto/domain/numberreceiver/TicketRepository.java
@@ -1,10 +1,12 @@
 package pl.lotto.domain.numberreceiver;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 
+@Repository
 public interface TicketRepository extends MongoRepository<Ticket, String> {
 
     Collection<Ticket> findAllTicketsByDrawDate(LocalDateTime drawDate);
